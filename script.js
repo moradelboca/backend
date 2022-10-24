46a609011ef02b97c1b3c053c9f9b16e6e7b2099
José Mora <jmora000@outlook.com>
2022-10-24 03:06:18 -0300

    Desafio clase 4- Manejo de archivos en JS

--- script.js
+++ script.js
@@ -1,35 +1,115 @@
-class Usuario{
-  constructor (nombre, apellido,){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.libros = []
-    this.mascotas = []
+const fs = require('fs')
+
+class Container{
+  constructor(name, fileURL){
+    this.name = name
+    this.fileURL = fileURL
   }
-  getFullName(){
-    return `${this.nombre} ${this.apellido}`
+  async checkExistance(){
+    try{
+      await fs.promises.access(this.fileURL, fs.constants.R_OK | fs.constants.W_OK)
+      return
+    }
+    catch{
+      console.log('Error: File wasn\'t found.')
+      try{
+        await fs.promises.writeFile(this.fileURL, JSON.stringify([]))
+        console.log('File created successfully.')
+      }
+      catch{
+        console.log("Error: File couldn't be created.") 
+      }
+    }
   }
-  addMascota(nombreMascota){
-    this.mascotas.push(nombreMascota)
+  async getAll(){
+    await this.checkExistance()
+    try{
+      const data = await fs.promises.readFile(this.fileURL, 'utf-8')
+      return JSON.parse(data)
+    }
+    catch (err){
+      console.log(`Error: ${err}`)
+      // Just to avoid future errors...
+      return []
+    }
   }
-  countMascotas(){
-    return this.mascotas.length
+  async save(object){
+    await this.checkExistance()
+    const data = await this.getAll()
+    // Check if it's the first object...
+    let objectID = !data.length ? 1 : data[data.length-1].id+1
+    const newData = JSON.stringify([ ...data, {id:objectID, ...object } ])
+    try{
+      await fs.promises.writeFile(this.fileURL, newData)
+      return objectID
+    }
+    catch (err){
+      console.log(`Error: ${err}`)
+      return -1
+    }
   }
-  addBook(nombre, autor){
-    this.libros.push({ nombre:nombre, autor:autor })
+  async getById(objectID){
+    await this.checkExistance()
+    const data = await this.getAll()
+    return data.find( object => object.id == objectID) ?? -1
   }
-  getBookNames(){
-    return this.libros.map( book => book.nombre )
+  async deleteById(objectID){
+    await this.checkExistance()
+    const data = await this.getAll()
+    const newData = JSON.stringify(data.filter( object => object.id != objectID ))
+    try{
+      await fs.promises.writeFile(this.fileURL, newData)
+    }
+    catch (err){
+      console.log(`Error: ${err}`)
+    }
+  }
+  async deleteAll(){
+    await this.checkExistance()
+    try{
+      await fs.promises.writeFile(this.fileURL, JSON.stringify([]))
+    }
+    catch (err){
+      console.log(`Error: ${err}`)
+    }
   }
 }
 
-const user = new Usuario("Elon", "Musk")
 
-user.addMascota("perro")
-user.addMascota("gato")
-console.log(user.countMascotas())
+const test = async () => {
+  // Create a products countainer
+  console.log('1-')
+  productsContainer = new Container("products", "./products.txt")
+  // Getting all items just to check it's empty (please delete products file on first run, just for testing purposes)  
+  let products = await productsContainer.getAll() 
+  console.log(products, '\n\n')
+  
+  // Adding 4 produts to the file
+  console.log('2-')
+  const prod1 = {name:'keyboard', price: 1200, thumbnail:'fakeurl.com'}
+  const prod2 = {name:'mouse', price: 1000, thumbnail:'fakeurl.com'}
+  const prod3 = {name:'display', price: 2000, thumbnail:'fakeurl.com'}
+  const prod4 = {name:'headset', price: 500, thumbnail:'fakeurl.com'}
+  await productsContainer.save(prod1)
+  await productsContainer.save(prod2)
+  await productsContainer.save(prod3)
+  await productsContainer.save(prod4)
+  // Checking if products were added correctly..
+  products = await productsContainer.getAll()
+  console.log(products, '\n\n')
 
-user.addBook("El señor de las moscas", "William Golding")
-user.addBook("Fundacion", "Isaac Asimov")
-console.log(user.getBookNames())
-
-console.log(user.getFullName())
+  //Getting one product only and then deleting it
+  console.log('3-')
+  const pro = await productsContainer.getById(2)
+  console.log(pro)
+  await productsContainer.deleteById(pro.id) 
+  products = await productsContainer.getAll() 
+  console.log(products, '\n\n')
+  
+  // Deleting all products
+  console.log('4-')
+  await productsContainer.deleteAll()
+  products = await productsContainer.getAll() 
+  console.log(products, '\n\n')
+}
+test()
