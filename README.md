6982c5915b5013de337108b5cfc5b592dbbb6c40
José Mora <jmora000@outlook.com>
2023-02-10 14:22:18 -0300

    Desafio 1 armado con readme incluido

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Author: Mora del Boca José Ignacio
 
-Entregas:
+Consigna: 
+
+![Consigna 1](https://ibb.co/b503S9q)
+![Consigna 2](https://ibb.co/ZhpVNXR)
---
+++ script.js
@@ -0,0 +1,42 @@
+class ProductManager{
+  constructor(){
+    this.products = []
+  }
+  addProduct(product){
+    // Check if code isnt repeated
+    if (this.products.some( item => item.code === product.code )){return false}
+
+    product.id = this.products.length + 1 // IMPORTANT! This code is only valid because we cant take out products. Needs to be refactored if necessary!
+    this.products.push(product)
+    return true
+  }
+  getProducts(){
+    return this.products
+  }
+  getProductById(id){
+    return this.products.find( product => product.id === id ) || 'Product wasnt found!'
+  }
+}
+
+class Product{
+  constructor(title, description, price, thumbnail, code, stock){
+    this.title = title
+    this.description = description
+    this.price = price
+    this.thumbnail = thumbnail
+    this.code = code
+    this.stock =  stock
+  }
+}
+
+const pm = new ProductManager()
+
+pm.addProduct(new Product('Collar', 'De perlas, negro', 1200, 'fakeurl.com', 1233, 7))
+pm.addProduct(new Product('Collar', 'De hilo, gris', 400, 'fakeurl.com', 456456454, 50))
+pm.addProduct(new Product('Pulsera', 'De escamas, blanco', 500, 'fakeurl.com', 1233, 5)) // This wont be added cause code is repeated!
+pm.addProduct(new Product('Pulsera', 'Cadena metalica con dije amarillo', 2500, 'fakeurl.com', 4433, 2))
+
+console.log( pm.getProducts() )
+
+console.log( pm.getProductById(2) )
+console.log( pm.getProductById(10) )
