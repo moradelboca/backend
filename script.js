07abe20948db8935e60d2d35ffa60d40078d2602
José Mora <jmora000@outlook.com>
2023-02-23 02:22:53 -0300

    Desafio terminado

--- script.js
+++ script.js
@@ -1,43 +1,114 @@
+import { debug } from 'console'
+import fs from 'fs'
+
 class ProductManager{
-  constructor(){
-    this.products = []
+  constructor(path){
+    this.path = path
   }
-  addProduct(product){
+  async addProduct(title, description, price, thumbnail, code, stock){
+    const products = await this.getProducts()
     // Check if code isnt repeated
-    if (this.products.some( item => item.code === product.code )){return false}
-
-    product.id = this.products.length + 1 // IMPORTANT! This code is only valid because we cant take out products. Needs to be refactored if necessary!
-    this.products.push(product)
-    return true
+    if (products.some( item => item.code === code )){return -1}
+    // Generating ID
+    let id = !products.length ? 1 : products[products.length-1].id+1
+    const newProducts = JSON.stringify([ ...products, {id, title, description, price, thumbnail, code, stock } ])
+    try{
+      await fs.promises.writeFile(this.path, newProducts)
+      return id
+    }
+    catch (err){
+      console.log(`${err}`)
+      return -1
+    }
   }
-  getProducts(){
-    return this.products
+  async getProducts(){
+    try{
+      const data = await fs.promises.readFile(this.path, 'utf-8')
+      return data ? JSON.parse(data) : []
+    }
+    catch (err){
+      console.log(`${err}`)
+      // Just to avoid future errors...
+      return []
+    }
   }
-  getProductById(id){
-    return this.products.find( product => product.id === id ) || 'Product wasnt found!'
+  async getProductByID(id){
+    const data = await this.getProducts()
+    return data.find( product => product.id == id)
+  }
+  async deleteProduct(id){
+    const data = await this.getProducts()
+    // Update data without the product that needs to be deleted.
+    const newData = JSON.stringify(data.filter( product => product.id != id ))
+    try{
+      await fs.promises.writeFile(this.path, newData)
+    }
+    catch (err){
+      console.log(`${err}`)
+    }
+  }
+  async updateProduct(id, newPropierties){
+    // I think this is deficent... needs to be refactored!
+    const product = await this.getProductByID(id)
+    if (!product){return}
+    const newProduct = { ...product }
+    for (let property in newPropierties){
+      if(property in product && property != 'id'){newProduct[property] = newPropierties[property]}
+    }
+    const data = await this.getProducts()
+    const newData = JSON.stringify(data.map( object =>  {
+      return (object.id === id) ? newProduct : object
+    }))
+    try{
+      await fs.promises.writeFile(this.path, newData)
+    }
+    catch (err){
+      console.log(`${err}`)
+    }
+  }
+  async deleteAll(){
+    try{
+      await fs.promises.writeFile(this.path, JSON.stringify([]))
+    }
+    catch (err){
+      console.log(`Error: ${err}`)
+    }
   }
 }
 
-// Creating a class Product will avoid unnecesary atributes in each product. In this case, ill replace checking each if an input has all atributes using this concept.
-class Product{
-  constructor(title, description, price, thumbnail, code, stock){
-    this.title = title
-    this.description = description
-    this.price = price
-    this.thumbnail = thumbnail
-    this.code = code
-    this.stock =  stock
-  }
+async function test (){
+  // Create a products countainer
+  console.log('1- PM created')
+  const pm = new ProductManager('./static/products.json')
+  // Getting all items just to check it's empty
+  let products = await pm.getProducts() 
+  console.log(products, '\n\n')
+  
+  // Adding 4 produts to the file
+  await pm.addProduct('Titulo1', 'Descripcion1', 1000, 'fakeurl.com', 'I1MSMS2', 3)
+  await pm.addProduct('Titulo2', 'Descripcion2', 2000, 'fakeurl.com', 'IASD221', 10)
+  await pm.addProduct('Titulo3', 'Descripcion3', 3000, 'fakeurl.com', 'VVAS45E', 80)
+  await pm.addProduct('Titulo4', 'Descripcion4', 4000, 'fakeurl.com', 'DDWGHG3', 6)
+  await pm.addProduct('Titulo5', 'Descripcion5', 5000, 'fakeurl.com', 'RERY3FG', 2)
+  // This wont be added cause code is repeated!
+  await pm.addProduct('Titulo6', 'Descripcion6', 6000, 'fakeurl.com', 'RERY3FG', 100)
+  products = await pm.getProducts()
+  console.log('2- Products added')
+  console.log(products, '\n\n')
+  
+  // Deleting 3rd product
+  await pm.deleteProduct(3)
+  // Modifying 1st product title and description. Id wont be modified!
+  await pm.updateProduct(1, {id:1231231, title:'Nuevo Titulo', description:'Nueva Descripcion'})
+  products = await pm.getProducts()
+  console.log('3- Product 3 deleted and 1st modified')
+  console.log(products, '\n\n')
+  
+  // Deleting all products
+  pm.deleteAll()
+  products = await pm.getProducts()
+  console.log('3- All products have been deleted')
+  console.log(products, '\n\n')
 }
 
-const pm = new ProductManager()
-
-pm.addProduct(new Product('Collar', 'De perlas, negro', 1200, 'fakeurl.com', 1233, 7))
-pm.addProduct(new Product('Collar', 'De hilo, gris', 400, 'fakeurl.com', 456456454, 50))
-pm.addProduct(new Product('Pulsera', 'De escamas, blanco', 500, 'fakeurl.com', 1233, 5)) // This wont be added cause code is repeated!
-pm.addProduct(new Product('Pulsera', 'Cadena metalica con dije amarillo', 2500, 'fakeurl.com', 4433, 2))
-
-console.log( pm.getProducts() )
-
-console.log( pm.getProductById(2) )
-console.log( pm.getProductById(10) )
+test()
