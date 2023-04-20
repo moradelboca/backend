6755142e667ffd60c6fe7db1f7a2529753aa9b3e
José Mora <jmora000@outlook.com>
2023-04-20 02:46:04 -0300

    carts model terminado. CON EL UPDATE Y MERGE

--- src/dao/models/CartsModel.js
+++ src/dao/models/CartsModel.js
@@ -19,7 +19,7 @@ class CartsModel {
     this.#cartsDb = mongoose.model('carts', schemaCarts)
   }
   async getCarts (){
-    const carts = await this.#cartsDb.find().lean()
+    const carts = await this.#cartsDb.find().populate('products.product').lean()
     return carts
   }
   async addCart(cart) {
@@ -27,11 +27,27 @@ class CartsModel {
     return saved
   }
   async getCartByID(id){
-    const cart = await this.#cartsDb.findOne({ _id: id }).lean()
+    const cart = await this.#cartsDb.findOne({ _id: id }).populate('products.product').lean()
     return cart
   }
   async updateCart(id, products){
-    
+    const newCart = await this.#cartsDb.findOne({ _id: id })
+    if (!newCart){ return -1 }
+    // Merging 2 lists... Objects re going to be duplicated!
+    newCart.products = [ ...newCart.products, ...products ]
+    // Reducing duplicated objects
+    newCart.products = newCart.products.reduce( (allProducts, currentProduct) => {
+      // Check if product is added
+      const productIndex = allProducts.findIndex( p => p.product.toString() === currentProduct.product.toString() )
+      if (productIndex != -1) {
+        allProducts[productIndex].quantity += currentProduct.quantity
+        return [ ...allProducts ]
+      }
+      // Product isnt in list
+      return [ ...allProducts, currentProduct ]
+    }, [])
+    const updated = await newCart?.save()
+    return updated
   }
   async deleteAll() {
     await this.#cartsDb.deleteMany({})
@@ -39,10 +55,5 @@ class CartsModel {
     return 1
   }
 }
-
-schemaCarts.pre(/^find/, function (next) {
-  this.populate('products.product')
-  next()
-})
 
 export const cartsModel = new CartsModel()
